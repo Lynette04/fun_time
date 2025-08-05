87fdc38cbbec8308a92b10aa8034d3307c80e551
Lynette04 <lynak03@gmail.com>
2025-08-05 13:26:53 +0300

    deleted

--- towerofhanoi.py
+++
@@ -1,9 +0,0 @@
-def tower_of_hanoi(n,a,b,c):
-    if n==1:
-        print("Move disk 1 from rod  ",a,"to rod ",c)
-        return
-    tower_of_hanoi(n-1,a,c,b)
-    print("Move disk ",n,"from rod ",a,"to rod ",c)
-    tower_of_hanoi(n-1,b,a,c)
-
-tower_of_hanoi(3,"A","B","C")
