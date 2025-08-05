8b4fa25239ec6a52747bc0f6084535fa1b985f1a
Lynette04 <lynak03@gmail.com>
2025-08-05 13:17:43 +0300

    The bubble sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+#This is the bubble sort algorithm 
+def bubble_sort(array):
+    n=len(array)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if array[j]>array[j+1]:
+                array[j],array[j+1]=array[j+1],array[j]
+    return array
+
+array=[1,7,6,5,4,2]
+result= bubble_sort(array)
+print("sorted array: ",result)             
