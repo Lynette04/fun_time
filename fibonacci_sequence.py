d5dbb5eb282f12bc9625992c7c13ca7f6980c31f
Lynette04 <lynak03@gmail.com>
2025-08-05 13:20:13 +0300

    fibonacci sequence algorithm in python

---
+++ fibonacci_sequence.py
@@ -0,0 +1,14 @@
+#This is the famous fibonacci sequence in python
+def fib(n):
+    if n==0 or n==1:
+        return n
+    else: 
+        return fib(n-1)+ fib(n-2) #since we are getting the next fibonacci number, we add the previous two numbers.
+    
+print(fib(5))
+
+'''fib(5)=fib(4)+fib(3)
+   fib(4)=fib(3)+fib(2)
+   fib(3)=fib(2)+fib(1)
+   fib(2)=fib(1)+fib(0)'''
+    
